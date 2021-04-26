9fe12938ef77adee0b5e2afbdd866d88856d918b
Shaun Morrow <smorrow@tillster.com>
2021-04-26 21:27:30 +0100

    Up to date

--- market.go
+++ market.go
@@ -1,16 +1,34 @@
 package bittrex
 
-import "github.com/shopspring/decimal"
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
 
 type Market struct {
-	MarketCurrency     string          `json:"MarketCurrency"`
-	BaseCurrency       string          `json:"BaseCurrency"`
-	MarketCurrencyLong string          `json:"MarketCurrencyLong"`
-	BaseCurrencyLong   string          `json:"BaseCurrencyLong"`
+	MarketCurrency     string  `json:"MarketCurrency"`
+	BaseCurrency       string  `json:"BaseCurrency"`
+	MarketCurrencyLong string  `json:"MarketCurrencyLong"`
+	BaseCurrencyLong   string  `json:"BaseCurrencyLong"`
 	MinTradeSize       decimal.Decimal `json:"MinTradeSize"`
-	MarketName         string          `json:"MarketName"`
-	IsActive           bool            `json:"IsActive"`
-	Notice             string          `json:"Notice"`
-	IsSponsored        bool            `json:"IsSponsored"`
-	LogoUrl            string          `json:"LogoUrl"`
+	MarketName         string  `json:"MarketName"`
+	IsActive           bool    `json:"IsActive"`
+	IsRestricted       bool    `json:"IsRestricted"`
+	Notice             string  `json:"Notice"`
+	IsSponsored        bool    `json:"IsSponsored"`
+	LogoUrl            string  `json:"LogoUrl"`
+	Created            string  `json:"Created"`
+}
+
+
+type MarketV3 struct {
+	Symbol              string   `json:"symbol"`
+	BaseCurrencySymbol  string   `json:"baseCurrencySymbol"`
+	QuoteCurrencySymbol string   `json:"quoteCurrencySymbol"`
+	MinTradeSize        decimal.Decimal   `json:"minTradeSize"`
+	Precision           int32   `json:"precision"`
+	Status              string   `json:"status"`
+	CreatedAt           time.Time   `json:"createdAt"`
+	Notice              string   `json:"notice"`
+	ProhibitedIn        []string `json:"prohibitedIn"`
 }
